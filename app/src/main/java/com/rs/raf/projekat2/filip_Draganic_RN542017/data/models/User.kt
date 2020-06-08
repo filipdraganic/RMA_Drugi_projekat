49fd09af8724611360a93a337bddc46065bc88e6
filipdraganic <draganicfilip@gmail.com>
2020-06-08 07:32:37 +0200

    Delom zavrseno

--- app/src/main/java/com/rs/raf/projekat2/filip_Draganic_RN542017/data/models/User.kt
+++
@@ -1,7 +0,0 @@
-package com.rs.raf.projekat2.filip_Draganic_RN542017.data.models
-
-data class User(
-
-    val login: String
-
-)
--- app/src/main/java/com/rs/raf/projekat2/filip_Draganic_RN542017/presentation/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/rs/raf/projekat2/filip_Draganic_RN542017/presentation/viewmodel/UserViewModel.kt
@@ -1,9 +1,5 @@
 package com.rs.raf.projekat2.filip_Draganic_RN542017.presentation.viewmodel
 
-import androidx.lifecycle.ViewModel
-import com.rs.raf.projekat2.filip_Draganic_RN542017.data.models.User
-import io.reactivex.Observable
-
 class UserViewModel(){
 
     init{
